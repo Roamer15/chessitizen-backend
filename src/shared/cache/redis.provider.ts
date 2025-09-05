35f5062875924861387ef53c1bfa4dea81cf8669
Beleke Ian <ianseeksbeta@gmail.com>
2025-09-05 03:41:37 +0100

    Redis config fixe

--- src/shared/cache/redis.provider.ts
+++ src/shared/cache/redis.provider.ts
@@ -11,7 +11,7 @@ export const RedisProvider: Provider = {
       port: parseInt(process.env.REDIS_PORT || '6379'),
       username: process.env.REDIS_USERNAME || 'default',
       password: process.env.REDIS_PASSWORD,
-      tls: {},
+      tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
     });
   },
 };
